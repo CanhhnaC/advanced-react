c22fd7481bbdb1714b70c8f03a4213b6c84088ea
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-20 10:02:31 +0700

    Full-stack JavaScripit with React.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,28 @@
 const path = require('path');
-const webpack = require('webpack');
 
 const config = {
   resolve: {
-    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')],
   },
-  entry: {
-    vendor: [
-      'babel-polyfill',
-      'react',
-      'react-dom',
-      'prop-types',
-      'axios',
-      'lodash.debounce',
-      'lodash.pickby'
-    ],
-    app: ['./lib/renderers/dom.js']
-  },
+  entry: ['babel-polyfill', './lib/components/index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    filename: 'bundle.js',
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'env', 'stage-2']
-        }
-      }
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2'],
+          },
+        },
+      },
+    ],
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ]
 };
 
 module.exports = config;
