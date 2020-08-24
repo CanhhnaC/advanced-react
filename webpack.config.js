23068a6fb7d2ee42759fb37a08d0601d17d3d329
Nguyen Tran Canh <nguyentrancanh81@gmail.com>
2020-08-24 20:03:51 +0700

    fix something

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const config = {
   resolve: {
     modules: [path.resolve('./lib'), path.resolve('./node_modules')],
   },
-  entry: ['babel-polyfill', './lib/components/index.js'],
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
